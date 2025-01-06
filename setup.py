4be1dad1b2996b5a03106ae68c49e5d00216aafd
print-sid8 <sidsub94@gmail.com>
2025-01-06 15:03:16 +0530

    increase version for minor mistakes in readme and py versions

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open("requirements.txt", "r", encoding="utf-8") as fh:
 
 setup(
     name="rasteret",
-    version="0.1.0",
+    version="0.1.1",
     author="Sidharth Subramaniam",
     author_email="sid@terrafloww.com",
     description="Fast and efficient access to Cloud-Optimized GeoTIFFs (COGs)",
