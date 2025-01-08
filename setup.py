26946e331731ef31fba3f679129cb6f50bdb3aa5
print-sid8 <sidsub94@gmail.com>
2025-01-08 17:06:08 +0530

    minor version bump to update pypi

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("requirements.txt", "r", encoding="utf-8") as fh:
 
 setup(
     name="rasteret",
-    version="0.1.4",
+    version="0.1.5",
     author="Sidharth Subramaniam",
     author_email="sid@terrafloww.com",
     description="Fast and efficient access to Cloud-Optimized GeoTIFFs (COGs)",
