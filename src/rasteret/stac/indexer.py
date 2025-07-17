3ff9d7b445b0d1cacc4ccbbef82bfd81d5dc0a1a
print-sid8 <sidsub94@gmail.com>
2025-07-18 00:42:43 +0530

    perf: increase indexer concurrency limits

--- src/rasteret/stac/indexer.py
+++ src/rasteret/stac/indexer.py
@@ -51,7 +51,7 @@ class StacToGeoParquetIndexer:
         name: Optional[str] = None,
         cloud_provider: Optional[CloudProvider] = None,
         cloud_config: Optional[CloudConfig] = None,
-        max_concurrent: int = 100,
+        max_concurrent: int = 300,  # Increased from 100
     ):
         self.data_source = data_source
         self.stac_api = stac_api
@@ -60,7 +60,7 @@ class StacToGeoParquetIndexer:
         self.cloud_config = cloud_config
         self.name = name
         self.max_concurrent = max_concurrent
-        self.batch_size = 50
+        self.batch_size = 100  # Increased from 50
 
     @property
     def band_map(self) -> Dict[str, str]:
